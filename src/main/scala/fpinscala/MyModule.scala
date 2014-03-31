39717ed572e7ab23f90b6da16979c68186064aee
Jordi Pradel <jordi.pradel@agilogy.com>
2014-03-31 08:14:39 +0200

    ex 02.02

--- src/main/scala/fpinscala/MyModule.scala
+++ src/main/scala/fpinscala/MyModule.scala
@@ -4,28 +4,49 @@ import scala.annotation.tailrec
 
 /** A documentation comment */
 object MyModule {
-   def abs(n: Int): Int =
-     if (n < 0) -n
-     else n
+  def abs(n: Int): Int =
+    if (n < 0) -n
+    else n
 
-   private def formatAbs(x: Int) = {
-     val msg = "The absolute value of %d is %d"
-     msg.format(x, abs(x))
-   }
+  private def formatAbs(x: Int) = {
+    val msg = "The absolute value of %d is %d"
+    msg.format(x, abs(x))
+  }
 
-   def main(args: Array[String]): Unit =
-     println(formatAbs(-42))
+  def main(args: Array[String]): Unit =
+    println(formatAbs(-42))
 
-   def factorial(n: Int): Int = {
-     @tailrec
-     def go(n: Int, acc: Int): Int =
-       if (n <= 0) acc
-       else go(n - 1, n * acc)
-     go(n, 1)
-   }
+  def factorial(n: Int): Int = {
+    @tailrec
+    def go(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else go(n - 1, n * acc)
+    go(n, 1)
+  }
 
-   def fib(n: Int): Int =
-     if (n == 0) 0
-     else if (n == 1) 1
-     else fib(n-2) + fib(n-1)
- }
+  def fib(n: Int): Int =
+    if (n == 0) 0
+    else if (n == 1) 1
+    else fib(n - 2) + fib(n - 1)
+
+  def findFirst[A](as: Array[A], p: A => Boolean): Int = {
+    @tailrec
+    def loop(n: Int): Int =
+      if (n >= as.length) -1
+      else if (p(as(n))) n
+      else loop(n + 1)
+    loop(0)
+  }
+
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
+
+    @tailrec
+    def isSorted(from:Int):Boolean = {
+      if (as.size <= from + 1) true
+      else if (!gt(as(from+1), as(from))) false
+      else isSorted(from+1)
+    }
+
+    isSorted(0)
+  }
+}
--- src/test/scala/fpinscala/MyModuleSpec.scala
+++ src/test/scala/fpinscala/MyModuleSpec.scala
@@ -18,4 +18,22 @@ class MyModuleSpec extends FlatSpec{
     assert(MyModule.fib(6) === 8) //0 1 1 2 3 5 8
   }
 
+  behavior of "MyModule.isSorted"
+
+  it should "return true for an empty array" in {
+    assert(MyModule.isSorted[Int](Array.empty,_ > _) === true)
+  }
+
+  it should "return true for an array with only one element" in{
+    assert(MyModule.isSorted[Int](Array(23),_ > _) === true)
+  }
+
+  it should "return true for a sorted array with N elements" in{
+    assert(MyModule.isSorted[Int](Array(23, 42, 158),_ > _) === true)
+  }
+
+  it should "return false for a not sorted array with N elements" in{
+    assert(MyModule.isSorted[Int](Array(42, 23),_ > _) === false)
+  }
+
 }
