de373bae76ff89d7dc6e18ebe6587d933bfa5fde
Jordi Pradel <jordi.pradel@agilogy.com>
2014-03-31 08:28:50 +0200

    ch 02.6

--- src/main/scala/fpinscala/MyModule.scala
+++ src/main/scala/fpinscala/MyModule.scala
@@ -58,4 +58,8 @@ object MyModule {
 
   def compose[A,B,C](f: B => C, g: A => B): A => C = (a:A) => f(g(a))
 
+  def composeCheating1[A,B,C](f: B => C, g: A => B): A => C = f compose g
+
+  def composeCheating2[A,B,C](f: B => C, g: A => B): A => C = g andThen f
+
 }
