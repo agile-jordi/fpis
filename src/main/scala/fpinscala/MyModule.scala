bfd65fd2ba7f75a3e984d02e7d192c12795c737a
Jordi Pradel <jordi.pradel@agilogy.com>
2014-03-31 08:23:22 +0200

    ex 02.03

--- src/main/scala/fpinscala/MyModule.scala
+++ src/main/scala/fpinscala/MyModule.scala
@@ -49,4 +49,8 @@ object MyModule {
 
     isSorted(0)
   }
+
+  def partial1[A,B,C](a: A, f: (A,B) => C): B => C = (b: B) => f(a, b)
+
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) = (a:A) => (b:B) => f(a,b)
 }
