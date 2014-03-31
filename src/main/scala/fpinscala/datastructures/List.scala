f05aa3754c625f797cbd4793bc213a456a92a59d
Jordi Pradel <jordi.pradel@agilogy.com>
2014-03-31 20:38:58 +0200

    ch 3.3.1

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -46,4 +46,10 @@ object List {
     case _ => l
   }
 
+  def append[A](a1: List[A], a2: List[A]): List[A] =
+    a1 match {
+      case Nil => a2
+      case Cons(h, t) => Cons(h, append(t, a2))
+    }
+
 }
