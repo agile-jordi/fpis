9bab6ebfdfb7e9c94f8dcd7ef156a8f297557f71
Jordi Pradel <jordi.pradel@agilogy.com>
2014-05-07 20:43:12 +0200

    ex 3.25 (tailrec version)

--- src/main/scala/fpinscala/datastructures/Tree.scala
+++ src/main/scala/fpinscala/datastructures/Tree.scala
@@ -1,13 +1,25 @@
 package fpinscala.datastructures
 
+import scala.annotation.tailrec
+
 sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree{
 
-  def size[A](t:Tree[A]):Int = t match {
-    case Leaf(_) => 1
-    case Branch(l,r) => size(l) + size(r)
+  def size[A](tree:Tree[A]):Int = {
+
+    @tailrec
+    def loop(t1:Tree[A], acc:(List[Tree[A]], Int)):Int = {
+      (t1,acc) match {
+        case (Leaf(_),(Nil,s)) => s + 1
+        case (Leaf(_),(Cons(h,t),s)) => loop(h,(t,s+1))
+        case (Branch(l,r),(pt,s)) => loop(l,(Cons(r,pt),s))
+      }
+    }
+
+    loop(tree,(List(),0))
+
   }
 }
