872ab369b416a093e935a9191e6462eecd95e009
Jordi Pradel <jordi.pradel@agilogy.com>
2014-05-08 08:31:28 +0200

    Generalize 3.25, 3.26 and 3.27 to use fold (not tailrec)

--- src/main/scala/fpinscala/datastructures/Tree.scala
+++ src/main/scala/fpinscala/datastructures/Tree.scala
@@ -8,7 +8,7 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree{
 
-  def fold[A,B](tree:Tree[A], z:B)(f: (A,B) => B):B = {
+  def foldLeft[A,B](tree:Tree[A], z:B)(f: (A,B) => B):B = {
     @tailrec
     def loop(t1:Tree[A], acc:(List[Tree[A]], B)):B = {
       (t1,acc) match {
@@ -21,12 +21,14 @@ object Tree{
     loop(tree,(List(),z))
   }
 
-  def size[A](tree:Tree[A]):Int = fold(tree,0)((_,acc) => acc+1)
-
-  def max(tree:Tree[Int]):Int = fold[Int,Option[Int]](tree,None)((e,acc) => Some(e max acc.getOrElse(e))).get
-
-  def depth[A](tree:Tree[A]):Int = tree match{
-    case Leaf(_) => 1
-    case Branch(l,r) => (depth(l) max depth(r)) + 1
+  def fold[A,B](tree:Tree[A])(z:(A) => B)(f:(B,B)=>B):B = tree match {
+    case Leaf(a) => z(a)
+    case Branch(l,r) => f(fold(l)(z)(f),fold(r)(z)(f))
   }
+
+  def size[A](tree:Tree[A]):Int = fold(tree)(_ => 1)(_ + _)
+
+  def max(tree:Tree[Int]):Int = fold(tree)(l => l)(_ max _)
+
+  def depth[A](tree:Tree[A]):Int = fold[A,Int](tree)(_ => 1)((l,r) => (l max r) + 1)
 }
