b21482323d5fee8886bb9ac0ce4427d2a40adb89
Jordi Pradel <jordi.pradel@agilogy.com>
2014-06-04 19:25:09 +0200

    ex 4.5

--- src/main/scala/fpinscala/errors/Option.scala
+++ src/main/scala/fpinscala/errors/Option.scala
@@ -6,41 +6,66 @@ import scala.annotation.tailrec
 
 sealed trait Option[+A] {
   def map[B](f: A => B): Option[B]
+
   def flatMap[B](f: A => Option[B]): Option[B]
+
   def getOrElse[B >: A](default: => B): B
+
   def orElse[B >: A](ob: => Option[B]): Option[B]
+
   def filter(f: A => Boolean): Option[A]
 }
 
-object Option{
+object Option {
 
-  def lift[A,B](f: A => B): Option[A] => Option[B] = _ map f
+  def lift[A, B](f: A => B): Option[A] => Option[B] = _ map f
 
   def Try[A](a: => A): Option[A] =
     try Some(a)
-    catch { case NonFatal(_) => None }
+    catch {
+      case NonFatal(_) => None
+    }
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
-  a.flatMap(aa => b.map(bb => f(aa,bb)))
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a.flatMap(aa => b.map(bb => f(aa, bb)))
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = {
     @tailrec
-    def loop(a:List[Option[A]], acc:List[A]):Option[List[A]]  = a match {
+    def loop(a: List[Option[A]], acc: List[A]): Option[List[A]] = a match {
       case Nil => Some(List.reverse(acc))
-      case Cons(None,xs) => None
-      case Cons(Some(x),xs) => loop(xs,Cons(x,acc))
+      case Cons(None, xs) => None
+      case Cons(Some(x), xs) => loop(xs, Cons(x, acc))
     }
-    loop(a,List())
+    loop(a, List())
   }
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = {
+
+    @tailrec
+    def loop(l: List[A], acc: List[B]): Option[List[B]] = l match {
+      case Nil => Some(List.reverse(acc))
+      case Cons(a, as) => {
+        val ob = f(a)
+        ob match {
+          case Some(b) => loop(as, Cons(b, acc))
+          case None => None
+        }
+      }
+    }
+    loop(a, List())
+  }
+
+  def sequenceInTermsOfTraverse[A](a: List[Option[A]]): Option[List[A]] = traverse(a)(identity)
+
 }
 
-case class Some[A](v:A) extends Option[A] {
+case class Some[A](v: A) extends Option[A] {
 
   override def map[B](f: (A) => B) = Some(f(v))
 
   override def flatMap[B](f: (A) => Option[B]) = f(v)
 
-  override def filter(f: (A) => Boolean) = if(f(v)) this else None
+  override def filter(f: (A) => Boolean) = if (f(v)) this else None
 
   override def getOrElse[B >: A](default: => B) = v
 
--- src/test/scala/fpinscala/errors/OptionSpec.scala
+++ src/test/scala/fpinscala/errors/OptionSpec.scala
@@ -77,5 +77,40 @@ class OptionSpec extends FlatSpec{
     assert(sequence(List(Some(1),Some(2), Some(3))) === Some(List(1,2,3)))
   }
 
+  behavior of "traverse"
+
+  import Option.traverse
+
+  it should "return the empty list for the empty list" in {
+    assert(traverse(List.apply[String]())(_ => Some("Yay!")) === Some(List()))
+  }
+
+  private val evenMinusOne = (i:Int) => if (i%2 == 0) Some(i-1) else None
+
+  it should "traverse a non empty list without errors" in {
+    assert(traverse(List(2,4,6))(evenMinusOne) === Some(List(1,3,5)))
+  }
+
+  it should "return none if a single element returns none" in {
+    assert(traverse(List(2,5,6))(evenMinusOne) === None)
+  }
+
+  behavior of "sequence in terms of traverse"
+
+  import Option.sequenceInTermsOfTraverse
+
+  it should "return the empty list for the empty list" in{
+    assert(sequenceInTermsOfTraverse(List.apply[Option[String]]()) === Some(List()))
+  }
+
+  it should "return None if the list contains a None" in {
+    assert(sequenceInTermsOfTraverse(List(Some(1),Some(2), None, Some(3))) === None)
+  }
+
+  it should "return the list if all the elements in the list are defined" in {
+    assert(sequenceInTermsOfTraverse(List(Some(1),Some(2), Some(3))) === Some(List(1,2,3)))
+  }
+
+
 
 }
