9a9fbc75194efb0f6e524365fef377f27f545d5f
Jordi Pradel <jordi.pradel@agilogy.com>
2014-06-17 20:17:23 +0200

    ex 5.2

--- src/main/scala/fpinscala/strictness/Stream.scala
+++ src/main/scala/fpinscala/strictness/Stream.scala
@@ -2,7 +2,7 @@ package fpinscala.strictness
 
 import scala.annotation.tailrec
 
-sealed trait Stream[+A]{
+sealed trait Stream[+A] {
   def headOption: Option[A] = this match {
     case Empty => None
     case Cons(h, t) => Some(h())
@@ -11,16 +11,40 @@ sealed trait Stream[+A]{
   def toList: List[A] = {
 
     @tailrec
-    def loop(s:Stream[A], acc:List[A]):List[A] = s match{
+    def loop(s: Stream[A], acc: List[A]): List[A] = s match {
       case Empty => acc
-      case Cons(h,t) => loop(t(), h() :: acc)
+      case Cons(h, t) => loop(t(), h() :: acc)
     }
 
-    loop(this,List.empty[A])
+    loop(this, List.empty[A])
+  }
+
+  def take(n: Int): List[A] = {
+
+    @tailrec
+    def loop(s:Stream[A], i:Int, acc:List[A]):List[A] = s match {
+      case Cons(h,t) if i>0 => loop(t(),i-1,h() :: acc)
+      case _ => acc.reverse
+    }
+
+    loop(this,n,List.empty[A])
+
+  }
+
+  def drop(n: Int): Stream[A] = {
+
+    @tailrec
+    def loop(s:Stream[A], i:Int):Stream[A] = s match {
+      case Cons(h, t) if i > 0 => loop(t(),i-1)
+      case _ => s
+    }
+
+    loop(this,n)
   }
 }
 
 case object Empty extends Stream[Nothing]
+
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
 object Stream {
@@ -29,7 +53,9 @@ object Stream {
     lazy val tail = tl
     Cons(() => head, () => tail)
   }
+
   def empty[A]: Stream[A] = Empty
+
   def apply[A](as: A*): Stream[A] =
     if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
 }
--- src/test/scala/fpinscala/strictness/StreamSpec.scala
+++ src/test/scala/fpinscala/strictness/StreamSpec.scala
@@ -15,4 +15,32 @@ class StreamSpec extends FlatSpec{
     assert(cons(23,empty).toList === List(23))
   }
 
+  behavior of "take"
+
+  it should "return the empty list for the empty stream" in {
+    assert(empty.take(2) === List())
+  }
+
+  it should "return the first element of a non empty stream" in{
+    assert(cons(23,empty).take(1) === List(23))
+  }
+
+  it should "return the first elements if we ask more than it has" in{
+    assert(cons(23,cons(42,empty)).take(2) === List(23,42))
+  }
+
+  behavior of "drop"
+
+  it should "drop from the empty stream" in{
+    assert(empty.drop(3).toList === List.empty)
+  }
+
+  it should "drop from the non empty stream" in{
+    assert(cons(23,cons(43,empty)).drop(1).toList === List(43))
+  }
+
+  it should "dr8op from the non empty stream but only until it is empty" in{
+    assert(cons(23,empty).drop(5).toList === List())
+  }
+
 }
